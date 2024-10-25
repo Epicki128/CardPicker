15e7bf7bfa43ef6ed2465428b9e62cd9e09b1a9b
pudlo <pudlo@DESKTOP-K91CM2S>
2024-10-25 09:31:31 +0400

    Dodaj pliki projektów.

---
+++ CardPicker/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace CardPicker
+{
+    partial class Picker
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(206, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(385, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kliknij kartę, aby wyświetlić jej nazwę";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(48, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 225);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(189, 51);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(125, 225);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(337, 51);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(125, 225);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(484, 51);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(125, 225);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(631, 51);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(125, 225);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(43, 302);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(713, 46);
+            this.label2.TabIndex = 6;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(337, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Losuj ponownie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Picker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Picker";
+            this.Text = "Card Identifier";
+            this.Load += new System.EventHandler(this.Picker_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CardPicker/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardPicker
+{
+    public partial class Picker : Form
+    {
+        private string[] cards;
+        private string[] karty;
+        private int[] pickedCards = new int[5];
+
+        Random random = new Random();
+        public Picker()
+        {
+            InitializeComponent();
+
+            cards = new string[] {
+                "2_Clubs.jpg", "2_Diamonds.jpg", "2_Hearts.jpg", "2_Spades.jpg",
+    "3_Clubs.jpg", "3_Diamonds.jpg", "3_Hearts.jpg", "3_Spades.jpg",
+    "4_Clubs.jpg", "4_Diamonds.jpg", "4_Hearts.jpg", "4_Spades.jpg",
+    "5_Clubs.jpg", "5_Diamonds.jpg", "5_Hearts.jpg", "5_Spades.jpg",
+    "6_Clubs.jpg", "6_Diamonds.jpg", "6_Hearts.jpg", "6_Spades.jpg",
+    "7_Clubs.jpg", "7_Diamonds.jpg", "7_Hearts.jpg", "7_Spades.jpg",
+    "8_Clubs.jpg", "8_Diamonds.jpg", "8_Hearts.jpg", "8_Spades.jpg",
+    "9_Clubs.jpg", "9_Diamonds.jpg", "9_Hearts.jpg", "9_Spades.jpg",
+    "10_Clubs.jpg", "10_Diamonds.jpg", "10_Hearts.jpg", "10_Spades.jpg",
+    "Jack_Clubs.jpg", "Jack_Diamonds.jpg", "Jack_Hearts.jpg", "Jack_Spades.jpg",
+    "Queen_Clubs.jpg", "Queen_Diamonds.jpg", "Queen_Hearts.jpg", "Queen_Spades.jpg",
+    "King_Clubs.jpg", "King_Diamonds.jpg", "King_Hearts.jpg", "King_Spades.jpg",
+    "Ace_Clubs.jpg", "Ace_Diamonds.jpg", "Ace_Hearts.jpg", "Ace_Spades.jpg",
+    "Joker_Black.jpg", "Joker_Red.jpg" };
+
+            karty = new string[] {
+    "2 Trefl", "2 Karo", "2 Kier", "2 Pik",
+    "3 Trefl", "3 Karo", "3 Kier", "3 Pik",
+    "4 Trefl", "4 Karo", "4 Kier", "4 Pik",
+    "5 Trefl", "5 Karo", "5 Kier", "5 Pik",
+    "6 Trefl", "6 Karo", "6 Kier", "6 Pik",
+    "7 Trefl", "7 Karo", "7 Kier", "7 Pik",
+    "8 Trefl", "8 Karo", "8 Kier", "8 Pik",
+    "9 Trefl", "9 Karo", "9 Kier", "9 Pik",
+    "10 Trefl", "10 Karo", "10 Kier", "10 Pik",
+    "Walet Trefl", "Walet Karo", "Walet Kier", "Walet Pik",
+    "Dama Trefl", "Dama Karo", "Dama Kier", "Dama Pik",
+    "Król Trefl", "Król Karo", "Król Kier", "Król Pik",
+    "As Trefl", "As Karo", "As Kier", "As Pik",
+    "Joker Czarny", "Joker Czerwony"
+};
+
+            load_Images();
+        }
+
+        private void load_Images()
+        {
+            for (int i = 0, x = 0; i < 5; i++)
+            {
+                do
+                {
+                    x = random.Next(0, (cards.Length));
+                } while (pickedCards.Contains(x));
+                pickedCards[i] = x;
+            }
+
+
+            string basePath = Path.Combine(Application.StartupPath, "images");
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pictureBox1.Image = Image.FromFile(Path.Combine(basePath, cards[pickedCards[0]]));
+            pictureBox2.Image = Image.FromFile(Path.Combine(basePath, cards[pickedCards[1]]));
+            pictureBox3.Image = Image.FromFile(Path.Combine(basePath, cards[pickedCards[2]]));
+            pictureBox4.Image = Image.FromFile(Path.Combine(basePath, cards[pickedCards[3]]));
+            pictureBox5.Image = Image.FromFile(Path.Combine(basePath, cards[pickedCards[4]]));
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+            label2.Text = karty[pickedCards[2]];
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            label2.Text = karty[pickedCards[3]];
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            label2.Text = karty[pickedCards[0]];
+        }
+
+        private void Picker_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            label2.Text = karty[pickedCards[1]];
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            label2.Text = karty[pickedCards[4]];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            load_Images();
+        }
+    }
+}
